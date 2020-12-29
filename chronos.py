c66a63a5b47c6a700e9867a3be86b1764cb529e1
boris-shabash <boris.shabash@gmail.com>
2020-12-28 17:41:45 -0800

    managed to get the basic idea of changepoints, but still buggy

--- chronos.py
+++ chronos.py
@@ -23,7 +23,7 @@ import plotly.graph_objs as go
 import torch
 import pyro
 import pyro.distributions as dist
-from torch.optim import SGD, Rprop
+from torch.optim import SGD, Rprop, Adam
 from torch.distributions import constraints
 
 
@@ -49,6 +49,7 @@ class Chronos:
                  method="MLE", 
                  time_col = "ds",
                  target_col="y", 
+                 n_changepoints = 6,
                  year_seasonality_order=10,
                  month_seasonality_order=5,
                  weekly_seasonality_order=3,
@@ -58,6 +59,8 @@ class Chronos:
         self.method_ = method
         self.n_iter_ = max_iter
         self.lr_ = learning_rate
+        self.n_changepoints_ = n_changepoints
+        self.changepoint_range_  = 0.8
         
         
         self.year_seasonality_order_ = year_seasonality_order
@@ -74,7 +77,7 @@ class Chronos:
             A function to grab the raw data with a timestamp and a target column and
             add seasonality to the data using sine and cosine combinations. 
         '''
-        # make a copy to avoid side effects of changing the origina df
+        # make a copy to avoid side effects of changing the original df
         # and convert datetime column into time in days. This helps convergence
         # BY A LOT
         internal_data = data.copy()
@@ -86,12 +89,14 @@ class Chronos:
         internal_data[self.time_col_] = internal_data[self.time_col_].values.astype(float)/(1e9*60*60*24)
         
         
+        self.seasonality_cols_ = []
         # Yearly seasonality 
         for i in range(1, self.year_seasonality_order_+1):
             cycle_position = i*2*math.pi*internal_data['yearday']/366 # max value will be 365
                                                                       # since values will go from 0-365
             internal_data[f"yearly_sin_{i}"] = np.sin(cycle_position) 
             internal_data[f"yearly_cos_{i}"] = np.cos(cycle_position)
+            self.seasonality_cols_.extend([f"yearly_sin_{i}", f"yearly_cos_{i}"])
         
         
         # Monthly seasonality
@@ -100,6 +105,7 @@ class Chronos:
                                                                       # will go from 0 to 30
             internal_data[f"monthly_sin_{i}"] = np.sin(cycle_position) 
             internal_data[f"monthly_cos_{i}"] = np.cos(cycle_position)
+            self.seasonality_cols_.extend([f"monthly_sin_{i}", f"monthly_cos_{i}"])
         
         # Weekly seasonality
         for i in range(1, self.weekly_seasonality_order_+1):
@@ -107,20 +113,49 @@ class Chronos:
                                                                     # will go from 0 to 6
             internal_data[f"weekly_sin_{i}"] = np.sin(cycle_position) 
             internal_data[f"weekly_cos_{i}"] = np.cos(cycle_position) 
+            self.seasonality_cols_.extend([f"weekly_sin_{i}", f"weekly_cos_{i}"])
                                                                       
         
         # Add a constant so we can train a simple vector of coefficients
-        internal_data.insert(0, "CONST", 1.0)
+        #internal_data.insert(0, "CONST", 1.0)
         #display(internal_data)
         internal_data = internal_data.drop(['weekday', 'monthday', 'yearday'], axis=1)
+
+        X_trend = torch.tensor(internal_data[self.time_col_].values, dtype=torch.float32)
+        X_seasonality = torch.tensor(internal_data[self.seasonality_cols_].values , dtype=torch.float32)
+
+        if (self.target_col_ in internal_data.columns):
+            y = torch.tensor(internal_data[self.target_col_].values, dtype=torch.float32)
+        else:
+            y = None
         
         
         #assert(False)
-        return internal_data
+        return X_trend, X_seasonality, y
         
         
+    def find_changepoint_positions(self, X_trend):
         
+        changepoint_range = int(self.changepoint_range_ * X_trend.shape[0])
+
+        changepoints =  np.round(np.linspace(0, changepoint_range, self.n_changepoints_+1, dtype=np.float32))
+        changepoints = changepoints[1:] # The first entry will always be 0, but we don't
+                                        # want a changepoint right in the beginning
+
+        return torch.tensor(changepoints, dtype=torch.float32)
         
+
+    def make_A_matrix(self, point_number, changepoints):
+        A = torch.zeros((point_number, self.n_changepoints_))
+
+        for t, row in enumerate(A):
+            for j, col in enumerate(row):
+                if (changepoints[j] <= t):
+                    A[t, j] = 1.0
+
+        return A
+
+
     def fit(self, data):
         '''
             Fits the model to the data using the method specified.
@@ -130,16 +165,30 @@ class Chronos:
         self.history = data.copy()
         
         # Transform the data by adding seasonality
-        internal_data = self.transform_data_(data)
+        #internal_data = self.transform_data_(data)
+        X_trend, X_seasonality, y = self.transform_data_(data)
+        
+        
+        self.changepoints = self.find_changepoint_positions(X_trend)
+        A = self.make_A_matrix(X_trend.shape[0], self.changepoints)
+
+        #print(self.changepoints)
+        #print(A)
+        
+        
+        #for t, row in enumerate(A):
+        #    print(t, row)
+        #assert(False)
+            
         
         # Split the data into X features (all time related features), and 
         # the target y
         
-        y = torch.tensor(internal_data[self.target_col_].values, dtype=torch.float32)
+        #y = torch.tensor(internal_data[self.target_col_].values, dtype=torch.float32)
         
-        internal_data = internal_data.drop(self.target_col_, axis=1)
-        X = torch.tensor(internal_data.values, dtype=torch.float32)
-        self.X_names = internal_data.columns.values
+        #internal_data = internal_data.drop(self.target_col_, axis=1)
+        #X = torch.tensor(internal_data.values, dtype=torch.float32)
+        #self.X_names = internal_data.columns.values
         
         
         
@@ -161,7 +210,9 @@ class Chronos:
                 warnings.simplefilter("ignore")
                 self.train_point_estimate(self.model, 
                                           self.guide,
-                                          X,
+                                          X_trend,
+                                          X_seasonality,
+                                          A,
                                           y)
         elif (self.method == "MCMC"):
             print("Employing Markov Chain Monte Carlo")
@@ -173,26 +224,29 @@ class Chronos:
         pass
     
     ##################################################################
-    def train_point_estimate(self, model, guide, X, y):
+    def train_point_estimate(self, model, guide, X_trend, X_seasonality, A, y):
         
         pyro.clear_param_store()
         
+        # Adam, SGD
         optimizer = Rprop
         scheduler = pyro.optim.ExponentialLR({'optimizer': optimizer, 
                                               'optim_args': {'lr': self.lr_}, 
                                               'gamma': 0.5})
         
+        my_loss = JitTrace_ELBO()
+        my_loss = Trace_ELBO()
         self.svi_ = SVI(model, 
                         guide, 
                         scheduler, 
-                        loss=JitTrace_ELBO())
+                        loss=my_loss)
         
         
         print_interval = max(self.n_iter_//10000, 10)
         
         for step in range(self.n_iter_):
             
-            loss = round(self.svi_.step(X, y)/y.shape[0], 4)
+            loss = round(self.svi_.step(X_trend, X_seasonality, A, y)/y.shape[0], 4)
             
             if (step % print_interval == 0):
                 pct_done = round(100*(step+1)/self.n_iter_, 2)
@@ -206,16 +260,22 @@ class Chronos:
             
     ##################################################################
     
-    def model_MLE_(self, X, y=None):            
-        #mu = 0
-        #for i in range(0, X.size(1)):
-        #    beta_1 = pyro.param(f"beta_{self.X_names[i]}", torch.tensor(0.0))
-        #    
-        #    mu += (beta_1 * X[:, i])
-        betas = pyro.param(f"betas", torch.zeros(X.size(1)))
-        #betas = betas.view(-1,1)
+    def model_MLE_(self, X_trend, X_seasonality, y=None):     
+
+        intercept = pyro.param("intercept", torch.tensor(0.0))
+        trend_slope = pyro.param("trend_slope", torch.tensor(0.0))
+
+        trend = trend_slope * X_trend + intercept
+
+        betas = pyro.param(f"betas", torch.zeros(X_seasonality.size(1)))
+
+                                        
         
-        mu = X.matmul(betas)
+        seasonality = X_seasonality.matmul(betas)
+        
+
+        mu = trend + seasonality
+
             
         
         sigma = pyro.param("sigma", 
@@ -223,26 +283,93 @@ class Chronos:
                            constraint = constraints.positive)
          
         
-        with pyro.plate("data", X.size(0)):
+        with pyro.plate("data", X_trend.size(0)):
             pyro.sample("obs", dist.Normal(mu, sigma), obs=y)
             
         return mu
         
         
     
-    def guide_MLE_(self, X, y=None):
+    def guide_MLE_(self, X_trend, X_seasonality, y=None):
         pass
     
     
     ##################################################################
+    def get_trend(self, X_trend):
+        intercept = pyro.sample("intercept", dist.Normal(0.0, 10.0))
+        trend_slope = pyro.sample("trend_slope", dist.Normal(0.0, 10.0))
+
+        trend = trend_slope * X_trend + intercept
+
+        return trend
+    ##################################################################
     
-    def model_MAP_(self, X, y=None):
+    def model_MAP_(self, X_trend, X_seasonality, A, y=None):
         
         
-        betas = pyro.sample(f"betas", dist.Normal(torch.zeros(X.size(1)), 
-                                                  torch.full((X.size(1),), 10.0)).to_event(1))
+        #print("X_trend", X_trend.shape)
+        #print("A", A.shape)
+        #trend = self.get_trend(X_trend)
+        intercept_init = pyro.sample("intercept", dist.Normal(0.0, 10.0)) # m
+        slope_init = pyro.sample("trend_slope", dist.Normal(0.0, 10.0)) # k
+
+        deltas = pyro.sample("delta", dist.Laplace(torch.zeros(self.n_changepoints_), 
+                                                   torch.full((self.n_changepoints_, ), 0.05)).to_event(1))
+
+        #deltas = torch.where(torch.abs(deltas) < 0.01, torch.tensor(0.0), deltas)
+        #print(deltas)
+
+        #gammas = pyro.param("gamma", torch.zeros(self.n_changepoints_))     
+        #gammas = torch.zeros_like(deltas)     
+        gammas = -deltas * X_trend[self.changepoints.type(torch.int64)]
+        '''for i in range(gammas.shape[0]):
+            #one = (self.changepoints[i] - intercept_init - gammas[:i].sum())
+            #two = 1 - (slope_init + deltas[:i].sum())/(slope_init + deltas[:i+1].sum())
+            one = -self.changepoints[i]
+            two = deltas[:i+1].sum() + slope_init
+            gammas[i] = one * two
+        #print(gammas)#'''
+
+        #gammas = pyro.sample("gamma", dist.Delta(torch.ones(self.n_changepoints_)).to_event(1))
+
+
+        slope = slope_init + torch.matmul(A, deltas)
+        intercept = intercept_init + torch.matmul(A, gammas)
         
-        mu = X.matmul(betas)
+
+        #print("slope", slope.shape)
+        '''print("#"*30)
+        
+        print("slope-init, k", slope_init)
+        print('intercept, m', intercept_init)
+        print("delta", deltas)
+        print("gammas", gammas)
+        print("changepoint", self.changepoints)
+        print(self.changepoints[0]-2, self.changepoints[0]+1)
+        print("t", self.changepoints[0]-2, self.changepoints[0]+2)
+        #print("A", )#'''
+        
+        
+
+
+
+        trend = (slope * X_trend) + intercept
+        #trend = (slope_init + torch.matmul(A, deltas)) * X_trend + (torch.matmul(A, gammas) + intercept_init)
+        #print('trend', trend[self.changepoints[0]-2:self.changepoints[0]+2])
+
+        pyro.deterministic("trend", trend)
+
+
+
+        betas = pyro.sample(f"betas", dist.Normal(torch.zeros(X_seasonality.size(1)), 
+                                                  torch.full((X_seasonality.size(1),), 10.0)).to_event(1))
+
+                                        
+        
+        seasonality = X_seasonality.matmul(betas)
+        
+
+        mu = trend + seasonality
 
         
         
@@ -250,7 +377,7 @@ class Chronos:
         df = pyro.sample("df", dist.HalfCauchy(1.0))
          
         
-        with pyro.plate("data", X.size(0)):
+        with pyro.plate("data", X_trend.size(0)):
             #pyro.sample("obs", dist.Normal(mu, sigma), obs=y)
             pyro.sample("obs", dist.StudentT(df, mu, sigma), obs=y)
             
@@ -282,14 +409,16 @@ class Chronos:
     
     ##################################################################
     def predict(self, data, sample_number=100, ci_interval=0.95):
-        internal_data = self.transform_data_(data)
+        #internal_data = self.transform_data_(data)
         #display(internal_data)
+
+        X_trend, X_seasonality, y = self.transform_data_(data)
         
         
-        y = torch.tensor(internal_data[self.target_col_].values, dtype=torch.float32)
+        #y = torch.tensor(internal_data[self.target_col_].values, dtype=torch.float32)
         
-        internal_data = internal_data.drop(self.target_col_, axis=1)
-        X = torch.tensor(internal_data.values, dtype=torch.float32)
+        #internal_data = internal_data.drop(self.target_col_, axis=1)
+        #X = torch.tensor(internal_data.values, dtype=torch.float32)
 
         
         if (self.method_ in ["MAP", "MLE"]):
@@ -297,22 +426,27 @@ class Chronos:
             predictive = Predictive(model=self.model,
                                     guide=self.guide,
                                     num_samples=sample_number,
-                                    return_sites=("obs",)) 
+                                    return_sites=("obs", "trend")) 
 
-            samples = predictive(X)
+            A = self.make_A_matrix(X_trend.shape[0], self.changepoints)
+            samples = predictive(X_trend, X_seasonality, A)
             
             
             space_on_each_side = (1.0 - ci_interval)/2.0
             lower_ntile = int(len(samples['obs']) * space_on_each_side)
             upper_ntile = int(len(samples['obs']) * (1.0 - space_on_each_side))
             
+
+            trend = samples['trend'].squeeze()[0, :]
+            print(trend.shape)
             predictions = pd.DataFrame({"yhat": torch.mean(samples['obs'], 0).detach().numpy(),
                                         "yhat_lower": samples['obs'].kthvalue(lower_ntile, dim=0)[0].detach().numpy(),
-                                        "yhat_upper": samples['obs'].kthvalue(upper_ntile, dim=0)[0].detach().numpy()})
+                                        "yhat_upper": samples['obs'].kthvalue(upper_ntile, dim=0)[0].detach().numpy(),
+                                        "trend": trend.detach().numpy()})
             
             predictions[self.target_col_] = y.detach().numpy()
             predictions[self.time_col_] = data[self.time_col_]
-            return predictions[[self.time_col_, self.target_col_, 'yhat', 'yhat_upper', 'yhat_lower']]
+            return predictions[[self.time_col_, self.target_col_, 'yhat', 'yhat_upper', 'yhat_lower', 'trend']]
         else:
             raise NotImplementedError(f"Did not implement .predict for {self.method_}")
 
@@ -342,9 +476,7 @@ class Chronos:
 
         return future_df
     
-    ##################################################################
-    def get_trend(self):
-        pass
+    
     ##################################################################
     def compute_seasonality(self, param_pairs, numeric_values, cycle_period):
         seasonality = np.zeros_like(numeric_values, dtype=np.float64)
@@ -371,7 +503,7 @@ class Chronos:
     def get_seasonal_params(self, param_name, seasonality_name):
         seasonal_params = []
         for i, param in enumerate(pyro.param(param_name)):
-            if (seasonality_name in self.X_names[i]):
+            if (seasonality_name in self.seasonality_cols_[i]):
                 seasonal_params.append(param.item())
                 
         seasonal_params = np.array(seasonal_params).reshape(-1, 2)
@@ -417,15 +549,25 @@ class Chronos:
         return weekly_seasonality
     
     ##################################################################
-    def plot_components(self, predictions, residuals=False):
+    def plot_components(self, predictions, residuals=False, changepoint_threshold = 0.0):
         
         fig, axs = plt.subplots(5, 1, figsize=(15, 15))
 
-        const, growth = pyro.param(self.param_name_).detach().numpy()[:2]
+        const = pyro.param('AutoDelta.intercept').detach()
+        growth = pyro.param('AutoDelta.trend_slope').detach()
+        #const, growth = pyro.param(self.param_name_).detach().numpy()[:2]
         trend_X = predictions[self.time_col_]
-        trend_Y = growth * trend_X.values.astype(float)/(1e9*60*60*24) + const
+        trend_Y = predictions['trend'] #growth * trend_X.values.astype(float)/(1e9*60*60*24) + const
         predictions_start = predictions[predictions[self.target_col_].isna()][self.time_col_].min()
         axs[0].plot(trend_X, trend_Y, linewidth=3, c="green")
+        for index, changepoint in enumerate(self.changepoints):
+            
+            changepoint_as_index = int(changepoint.item())
+            changepoint_value = self.changepoints[index]
+            changepoint_date_value = trend_X.iloc[changepoint_as_index]
+
+            if (abs(changepoint_value) > changepoint_threshold):
+                axs[0].axvline(changepoint_date_value, c="black", linestyle="dotted")
         axs[0].axvline(datetime.date(predictions_start.year, predictions_start.month, predictions_start.day), c="black", linestyle="--")
         axs[0].set_xlabel('Date', size=18)
         axs[0].set_ylabel('Growth', size=18)
--- test_script.py
+++ test_script.py
@@ -10,15 +10,28 @@ my_ts_data['ds'] = pd.to_datetime(my_ts_data['ds'])
 
 
 
-my_chronos = Chronos(method="MAP", max_iter=1000, learning_rate=0.1)
+my_chronos = Chronos(method="MAP", max_iter=1000, learning_rate=10.0, n_changepoints=20)
 my_chronos.fit(my_ts_data)
 future_df = my_chronos.make_future_dataframe(period=365)
 
 predictions = my_chronos.predict(future_df, sample_number=1000)
+print(predictions)
+#assert(False)
+
 
 MAE = round(np.mean(np.abs(future_df['y'] - predictions['yhat'])), 2)
 
+plt.figure(figsize=(15,5))
+plt.plot(future_df['ds'], predictions['yhat'], c="green")
+plt.fill_between(future_df['ds'], predictions['yhat_upper'], predictions['yhat_lower'], color="green", alpha=0.3)
+plt.scatter(future_df['ds'], future_df['y'], c="black")
+plt.xlabel("Date", size=16)
+plt.ylabel("LOG(page_views)", size=16)
+plt.title(f"LOG(page view) for Peyton Manning from 2007-2016.\nMAE={MAE}", size=20)
+plt.savefig("Time Series v1 t-distribution.png", dpi=96*4)
+plt.show()#'''
 
+#assert(False)
 
 my_chronos.plot_components(predictions)
 
