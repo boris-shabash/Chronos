6b1461d552d55d5515fb17d6a6edf3fab510651d
boris-shabash <boris.shabash@gmail.com>
2021-01-19 20:15:34 -0800

    modified notebook to work as an example

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="chronos-forecast", 
-    version="0.0.5",
+    version="0.0.8",
     author="Boris Shabash",
     license="MIT",
     author_email="boris.shabash@gmail.com",
@@ -18,17 +18,18 @@ setup(
     package_dir={"": "chronos"},
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
-    python_requires='>=3.8',    
-    install_requires=["pandas>=1.1.5",
-                      "numpy>=1.19.2",
-                      "matplotlib>=3.2.2",
-                      "torch>=1.5.1",
-                      "pyro-ppl>=1.3.1"],
+    python_requires='>=3.7',    
+    install_requires=["pandas>=1.1",
+                      "numpy>=1.19",
+                      "matplotlib>=3.2",
+                      "torch>=1.5",
+                      "pyro-ppl>=1.3"],
     extras_require={
         "dev": ["pytest==6.2.1"]
     }
